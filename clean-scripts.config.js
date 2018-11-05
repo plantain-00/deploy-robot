01093868c4e39676c222b21a13f57b67382ab570
york yao <yaoao12306@outlook.com>
2018-11-05 20:39:57 +0800

    build: use type-coverage badge

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -16,7 +16,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles}`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'type-coverage -p src --at-least 89'
+    typeCoverage: 'type-coverage -p src'
   },
   test: [
     'tsc -p spec',
