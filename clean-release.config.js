5c622d068296c94ad5126802c8caca407b0d877b
york yao <yaoao12306@outlook.com>
2019-09-14 18:59:00 +0800

    chore: change release

--- clean-release.config.js
+++ clean-release.config.js
@@ -11,8 +11,13 @@ module.exports = {
   askVersion: true,
   releaseRepository: 'https://github.com/plantain-00/deploy-robot-release.git',
   postScript: [
-    'cd "[dir]" && rm -rf .git',
-    'cp Dockerfile "[dir]"',
-    'cd "[dir]" && docker build -t plantain/deploy-robot . && docker push plantain/deploy-robot'
+    // 'cd "[dir]" && rm -rf .git',
+    // 'cp Dockerfile "[dir]"',
+    // 'cd "[dir]" && docker build -t plantain/deploy-robot . && docker push plantain/deploy-robot'
+    'git add package.json',
+    'git commit -m "[version]"',
+    'git tag v[version]',
+    'git push',
+    'git push origin v[version]',
   ]
 }
